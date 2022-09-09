580bc547632a850d41028adc66ac1fea7576be9e
Saharsh Gupta <saharshg29@gmail.com>
2022-09-09 09:32:53 +0530

    3rd task finished

--- Q3/3.4.c
+++ Q3/3.4.c
@@ -1,47 +1,127 @@
-#include <stdio.h>>
-#include <conio.h>>
-char st[20];
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#define MAX 100
 int top = -1;
-void psh(char);
-char pop();
-int main()
+char stack[MAX];
+int isFull()
 {
-    char a[20], t;
-    int i, f = 1;
-    scanf("%s", &a);
-    for (i = 0; i < strlen(a); i++)
+    return top == MAX - 1;
+}
+int isEmpty()
+{
+    return top == -1;
+}
+void push(char item)
+{
+    if (isFull())
+        return;
+    top++;
+    stack[top] = item;
+}
+int pop()
+{
+    if (isEmpty())
+        return INT_MIN;
+    return stack[top--];
+}
+int peek()
+{
+    if (isEmpty())
+        return INT_MIN;
+    return stack[top];
+}
+int checkIfOperand(char ch)
+{
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+int precedence(char ch)
+{
+    switch (ch)
     {
-        if (a[i] == '(' || a[i] == '{' || a[i] == '[')
-            psh(a[i]);
-        if (a[i] == ')' || a[i] == '}' || a[i] == ']')
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+
+    case '^':
+        return 3;
+    }
+    return -1;
+}
+int getPostfix(char *expression)
+{
+    int i, j;
+    for (i = 0, j = -1; expression[i]; ++i)
+    {
+        if (checkIfOperand(expression[i]))
+            expression[++j] = expression[i];
+        else if (expression[i] == '(')
+            push(expression[i]);
+        else if (expression[i] == ')')
         {
-            if (top == -1)
-                f = 0;
+            while (!isEmpty(stack) && peek(stack) != '(')
+                expression[++j] = pop(stack);
+            if (!isEmpty(stack) && peek(stack) != '(')
+                return -1; // invalid expression
             else
-            {
-                t = pop();
-                if (a[i] == ')' && (t == '[' || t == '{'))
-                    f = 0;
-                if (a[i] == '}' && (t == '(' || t == '['))
-                    f = 0;
-                if (a[i] == ']' && (t == '{' || t == '('))
-                    f = 0;
-            }
+                pop(stack);
+        }
+        else // if an opertor
+        {
+            while (!isEmpty(stack) && precedence(expression[i]) <= precedence(peek(stack)))
+                expression[++j] = pop(stack);
+            push(expression[i]);
         }
     }
-    if (top >= 0)
-        f = 0;
-    if (f == 0)
-        printf("Unbalanced\n");
-    else
-        printf("Balanced\n");
+    while (!isEmpty(stack))
+        expression[++j] = pop(stack);
+    expression[++j] = '\0';
+}
+void reverse(char *exp)
+{
+    int size = strlen(exp);
+    int j = size, i = 0;
+    char temp[size];
+    temp[j--] = '\0';
+    while (exp[i] != '\0')
+    {
+        temp[j] = exp[i];
+        j--;
+        i++;
+    }
+    strcpy(exp, temp);
+}
+void brackets(char *exp)
+{
+    int i = 0;
+    while (exp[i] != '\0')
+    {
+        if (exp[i] == '(')
+            exp[i] = ')';
+        else if (exp[i] == ')')
+            exp[i] = '(';
+        i++;
+    }
+}
+void InfixtoPrefix(char *exp)
+{
+    int size = strlen(exp);
+    reverse(exp);
+    brackets(exp);
+    getPostfix(exp);
+    reverse(exp);
+}
+int main()
+{
+    printf("The infix is: ");
+    char expression[] = "((a/b)+c)-(d+(e*f))";
+    printf("%s\n", expression);
+    InfixtoPrefix(expression);
+    printf("The prefix is: ");
+    printf("%s\n", expression);
     return 0;
-}
-void psh(char a)
-{
-    st[++top] = a;
-}
-char pop()
-{
-    return st[top--];
 }
--- Q3/3.5.c
+++ Q3/3.5.c
@@ -1,23 +1,84 @@
-#include <stdio.h>
-void towers(int, char, char, char);
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 20 
+struct stack
+{
+char stk[MAX];
+int top;
+}s;
+void push(char item)
+{
+if (s.top == (MAX - 1))
+printf ("Stack is Full\n");
+else
+{
+s.top = s.top + 1; 
+s.stk[s.top] = item;
+}}
+void pop()
+{
+if (s.top == - 1)
+{
+printf ("Stack is Empty\n");
+}
+else
+{
+s.top = s.top - 1; 
+}}
 int main()
 {
-    int num;
-
-    printf("Enter the number of disks : ");
-    scanf("%d", &num);
-    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
-    towers(num, 'A', 'C', 'B');
-    return 0;
-}
-void towers(int num, char frompeg, char topeg, char auxpeg)
+char exp[MAX];
+int i = 0;
+s.top = -1;
+printf("\nINPUT THE EXPRESSION : ");
+scanf("%s", exp);
+for(i = 0;i < strlen(exp);i++)
 {
-    if (num == 1)
-    {
-        printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg);
-        return;
-    }
-    towers(num - 1, frompeg, auxpeg, topeg);
-    printf("\n Move disk %d from peg %c to peg %c", num, frompeg, topeg);
-    towers(num - 1, auxpeg, topeg, frompeg);
+if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{')
+{
+push(exp[i]); 
+continue;
+}
+else if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}') 
+{
+if(exp[i] == ')')
+{
+if(s.stk[s.top] == '(')
+{
+pop();
+}
+else
+{
+printf("\nUNBALANCED EXPRESSION\n");
+break;
+}}
+if(exp[i] == ']')
+{
+if(s.stk[s.top] == '[')
+{
+pop(); 
+}
+else
+{
+printf("\nUNBALANCED EXPRESSION\n");
+break;
+}}
+if(exp[i] == '}')
+{
+if(s.stk[s.top] == '{')
+{
+pop(); 
+}
+else
+{
+printf("\nUNBALANCED EXPRESSION\n");
+break;
+}}}}
+if(s.top == -1)
+{
+printf("\nBALANCED EXPRESSION\n");
+} else {
+    printf("UNBALANCED EXPRESSION");
+}
 }
